c67748fd623aa23f9ef413f369a54eeb12167a80
sparkpoints <sparkpoints@gmail.com>
2021-01-29 07:58:40 +0800

    Final:bakcup for PhD data analyze

--- radiomics/pandastest.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os,sys
-
-if __name__ == "__main__":
-    basepath = '/Users/yang/mybin/005Radiomics/Pyrex/PyRF_data/'
-    csvfilelist = os.listdir(basepath)
-    print(csvfilelist[0])
-    data = pd.read_csv(os.path.join(basepath, csvfilelist[0]), header=0)
-    print('here')
